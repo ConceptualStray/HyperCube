d240dfcd007e897cefe0dae24343d6fbb3d0da12
ConceptualStray <ConceptualStray@gmail.com>
2025-02-12 16:28:00 +0100

    setup

---
+++ main.lua
@@ -0,0 +1,17 @@
+eDebug = false
+if arg[2] == "debug" then
+    eDebug = true
+    require("lldebugger").start()
+end
+
+
+
+local love_errorhandler = love.errorhandler
+
+function love.errorhandler(msg)
+    if lldebugger then
+        error(msg, 2)
+    else
+        return love_errorhandler(msg)
+    end
+end
